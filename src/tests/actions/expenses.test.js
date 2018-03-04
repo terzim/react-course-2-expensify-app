3c2c3221bd2a3ae41b5fab081a1d7e22ce6f3f47
Massi <terzimassimiliano@gmail.com>
2018-03-04 17:47:09 +0000

    Set expenses from firebase

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,10 +1,19 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk]);
+
+beforeEach((done) => {
+	const expensesData = {};
+	expenses.forEach(({ id, description, note, amount, createdAt}) => {
+		expensesData[id] = { description, note, amount, createdAt};
+	});
+
+	database.ref('expenses').set(expensesData).then(() => done());
+});
 
 test('should setup remove expense action object', () => {
 	const action = removeExpense({ id: '123abc' });
@@ -73,7 +82,7 @@ test('should add expense to database and store', (done) => {
 	});
 });
 
-test('should add expense with defaults to database and store', () => {
+test('should add expense with defaults to database and store', (done) => {
 	const store = createMockStore({});
 	const expenseDefaults = {
 		description: '',
@@ -94,6 +103,26 @@ test('should add expense with defaults to database and store', () => {
 		return database.ref(`expenses/${actions[0].expense.id}`).once('value');
 	}).then((snapshot) => {
 		expect(snapshot.val()).toEqual(expenseDefaults);
+		done();
+	});
+});
+
+test('should setup set expense action object with data', () => {
+	const action = setExpenses(expenses);
+	expect(action).toEqual({
+		type: 'SET_EXPENSES',
+		expenses
+	});
+});
+
+test('should fetch the expenses from firebase', (done) => {
+	const store = createMockStore({});
+	store.dispatch(startSetExpenses()).then(() => {
+		const actions = store.getActions();
+		expect(actions[0]).toEqual({
+			type: 'SET_EXPENSES',
+			expenses
+		});
 		done();
 	});
 });
--- src/tests/fixtures/expenses.js
+++ src/tests/fixtures/expenses.js
@@ -1,31 +1,31 @@
 import moment from 'moment';
 
 export default [{
-	id: 1,
+	id: "1",
 	description: 'Gum',
 	note: '',
 	amount: 195,
 	createdAt: 0
 }, {
-	id: 2,
+	id: "2",
 	description: 'Rent',
 	note: '',
 	amount: 3000,
 	createdAt: moment(0).subtract(4, 'days').valueOf()
 }, {
-	id: 3,
+	id: "3",
 	description: 'Credit Card',
 	note: 'The credit card payment',
 	amount: 4000,
 	createdAt: moment(0).add(4, 'days').valueOf()
 }, {
-	id: 4,
+	id: "4",
 	description: 'Gym',
 	note: '',
 	amount: 10,
 	createdAt: moment(0).subtract(8, 'days').valueOf()
 }, {
-	id: 5,
+	id: "5",
 	description: 'School',
 	note: '',
 	amount: 12,
